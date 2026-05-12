837d06183e096193de03d97b44cd7729760044ba
zenghaoy <zenghaoy@uoguelph.ca>
2026-05-12 21:26:27 +0800

    change about section and remove other sections

--- app.js
+++ app.js
@@ -12,14 +12,18 @@ Retrieved January 15, 2022, from https://www.youtube.com/watch?v=QGVXmoZWZuw.
 *******************************************************************************************************************/
 
 /* Main Effect */
-const hamburger = document.querySelector('.header .nav-bar .nav-list .hamburger');
-const mobile_menu = document.querySelector('.header .nav-bar .nav-list ul');
-const menu_item = document.querySelectorAll('.header .nav-bar .nav-list ul li a');
-const header = document.querySelector('.header .container');
+const hamburger = document.querySelector(
+  ".header .nav-bar .nav-list .hamburger"
+);
+const mobile_menu = document.querySelector(".header .nav-bar .nav-list ul");
+const menu_item = document.querySelectorAll(
+  ".header .nav-bar .nav-list ul li a"
+);
+const header = document.querySelector(".header .container");
 
-hamburger.addEventListener('click', ()=>{
-    hamburger.classList.toggle('active');
-    mobile_menu.classList.toggle('active');
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  mobile_menu.classList.toggle("active");
 });
 /*
 document.addEventListener('scroll', ()=>{
@@ -32,10 +36,10 @@ document.addEventListener('scroll', ()=>{
 });
 */
 menu_item.forEach((item) => {
-	item.addEventListener('click', () => {
-		hamburger.classList.toggle('active');
-		mobile_menu.classList.toggle('active');
-	});
+  item.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    mobile_menu.classList.toggle("active");
+  });
 });
 
 /* End Main Effect */
@@ -45,79 +49,79 @@ menu_item.forEach((item) => {
 const card1 = document.querySelector(".card__inner1");
 
 card1.addEventListener("click", function (e) {
-  card1.classList.toggle('is-flipped');
+  card1.classList.toggle("is-flipped");
 });
 
 const card2 = document.querySelector(".card__inner2");
 
 card2.addEventListener("click", function (e) {
-  card2.classList.toggle('is-flipped');
+  card2.classList.toggle("is-flipped");
 });
 
 const card3 = document.querySelector(".card__inner3");
 
 card3.addEventListener("click", function (e) {
-  card3.classList.toggle('is-flipped');
+  card3.classList.toggle("is-flipped");
 });
 
 const card4 = document.querySelector(".card__inner4");
 
 card4.addEventListener("click", function (e) {
-  card4.classList.toggle('is-flipped');
+  card4.classList.toggle("is-flipped");
 });
 
 /* End Flip-Card Effect */
 
-
 /* Typewriter Effect */
 
-const textDisplay = document.getElementById('text')
-const phrases = ['Hello, my name is Ray.', 'I am a Software Developer.', 'I look forward to working with you in the near future!']
-let i = 0
-let j = 0
-let currentPhrase = []
-let isDeleting = false
-let isEnd = false
+const textDisplay = document.getElementById("text");
+const phrases = [
+  "Hello, my name is Ray.",
+  "I am a Software Developer.",
+  "I look forward to working with you in the future!",
+];
+let i = 0;
+let j = 0;
+let currentPhrase = [];
+let isDeleting = false;
+let isEnd = false;
 
 function loop() {
-    isEnd = false
-    textDisplay.innerHTML = currentPhrase.join('')
-    if (i < phrases.length) {
-        if (!isDeleting && j <= phrases[i].length) {
-            currentPhrase.push(phrases[i][j])
-            j++
-            textDisplay.innerHTML = currentPhrase.join('')
-        }
-
-        if (isDeleting && j <= phrases[i].length) {
-            currentPhrase.pop(phrases[i][j])
-            j--
-            textDisplay.innerHTML = currentPhrase.join('')
-        }
-
-        if (j == phrases[i].length) {
-            isEnd = true
-            isDeleting = true
-        }
-
-        if (isDeleting && j === 0) {
-            currentPhrase = []
-            isDeleting = false
-            i++
-            if (i == phrases.length) {
-                i = 0
-            }
-        }
-
+  isEnd = false;
+  textDisplay.innerHTML = currentPhrase.join("");
+  if (i < phrases.length) {
+    if (!isDeleting && j <= phrases[i].length) {
+      currentPhrase.push(phrases[i][j]);
+      j++;
+      textDisplay.innerHTML = currentPhrase.join("");
     }
-    const spedUp = Math.random() * (80 - 50) + 50
-    const normalSpeed = Math.random() * (300 - 200) + 200
-    const time = isEnd ? 2000 : isDeleting ? spedUp : normalSpeed
-    setTimeout(loop, time)
+
+    if (isDeleting && j <= phrases[i].length) {
+      currentPhrase.pop(phrases[i][j]);
+      j--;
+      textDisplay.innerHTML = currentPhrase.join("");
+    }
+
+    if (j == phrases[i].length) {
+      isEnd = true;
+      isDeleting = true;
+    }
+
+    if (isDeleting && j === 0) {
+      currentPhrase = [];
+      isDeleting = false;
+      i++;
+      if (i == phrases.length) {
+        i = 0;
+      }
+    }
+  }
+  const spedUp = Math.random() * (80 - 50) + 50;
+  const normalSpeed = Math.random() * (300 - 200) + 50;
+  const time = isEnd ? 2000 : isDeleting ? spedUp : normalSpeed;
+  setTimeout(loop, time);
 }
 
-loop()
-
-
+loop();
 
 /* End TypeWriter Effect */
